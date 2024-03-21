768f2c16fbefd2429512e345eeeda372c74004fe
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2024-03-21 14:41:54 +0000

    paterne

---
+++ ikirezi-platform/script2.js
@@ -0,0 +1,17 @@
+// script2.js
+
+var communeOptions = {
+    // Define your commune options here
+};
+
+function updateCommunes(communeSelectId, provinceSelectId) {
+    // Your updateCommunes function code here
+}
+
+function changeLanguage() {
+    // Your changeLanguage function code here
+}
+
+// Initial population of communes on page load
+updateCommunes('commune_passport', 'province_passport');
+updateCommunes('commune_birth', 'province_birth');
